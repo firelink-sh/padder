adb4e41a4dd71b1d3f20d935dcdb6565373fda68
wilhelmagren <wilhelmagren98@gmail.com>
2025-07-19 00:56:46 +0200

    fix: benches naming and add 50k pads

--- benches/benchmarks/str_pad_left.rs
+++ benches/benchmarks/str_pad_left.rs
@@ -5,54 +5,65 @@ use std::hint::black_box;
 
 pub fn str_pad_10_left(c: &mut Criterion) {
     let width: usize = 10;
-    c.bench_function("str pad 10 la", |b| {
+    c.bench_function("&str.pad 10 la", |b| {
         b.iter(|| black_box("🤠".pad(width, Alignment::Left, 'µ')))
     });
     c.bench_function("format! 10 la", |b| {
-        b.iter(|| black_box(format!("{:<width$}", "🤠")))
+        b.iter(|| black_box(format!("{:<width$}", "🤠", width = width)))
     });
 }
 
 pub fn str_pad_100_left(c: &mut Criterion) {
     let width: usize = 100;
-    c.bench_function("str pad 100 la", |b| {
+    c.bench_function("&str.pad 100 la", |b| {
         b.iter(|| black_box("praise the sun!".pad(width, Alignment::Left, 'x')))
     });
     c.bench_function("format! 100 la", |b| {
-        b.iter(|| black_box(format!("{:<width$}", "praise the sun!")))
+        b.iter(|| black_box(format!("{:<width$}", "praise the sun!", width = width)))
     });
 
 }
 
 pub fn str_pad_1000_left(c: &mut Criterion) {
     let width: usize = 1000;
-    c.bench_function("str pad 1000 la", |b| {
+    c.bench_function("&str.pad 1000 la", |b| {
         b.iter(|| black_box("solaire is awesome".pad(width, Alignment::Left, '!')))
     });
     c.bench_function("format! 1000 la", |b| {
-        b.iter(|| black_box(format!("{:<width$}", "solaire is awesome")))
+        b.iter(|| black_box(format!("{:<width$}", "solaire is awesome", width = width)))
     });
 
 }
 
 pub fn str_pad_10000_left(c: &mut Criterion) {
-    let width: usize = 10000;
-    c.bench_function("str pad 10000 la", |b| {
+    let width: usize = 10_000;
+    c.bench_function("&str.pad 10000 la", |b| {
         b.iter(|| black_box("don't you dare go hollow..!!#\"".pad(width, Alignment::Left, 'ø')))
     });
     c.bench_function("format! 10000 la", |b| {
-        b.iter(|| black_box(format!("{:<width$}", "don't you dare go hollow..!!#\"")))
+        b.iter(|| black_box(format!("{:<width$}", "don't you dare go hollow..!!#\"", width = width)))
     });
 
 }
 
-pub fn str_pad_100000_left(c: &mut Criterion) {
-    let width: usize = 100000;
-    c.bench_function("str pad 100000 la", |b| {
+pub fn str_pad_25000_left(c: &mut Criterion) {
+    let width: usize = 25_000;
+    c.bench_function("&str.pad 25000 la", |b| {
         b.iter(|| black_box("東風 ぬが ㅀㆈ".pad(width, Alignment::Left, 'µ')))
     });
-    c.bench_function("format! 100000 la", |b| {
-        b.iter(|| black_box(format!("{:<width$}", "東風 ぬが ㅀㆈ")))
+    c.bench_function("format! 25000 la", |b| {
+        b.iter(|| black_box(format!("{:<width$}", "東風 ぬが ㅀㆈ", width = width)))
+    });
+}
+
+
+pub fn str_pad_50000_left(c: &mut Criterion) {
+    let width: usize = 50_000;
+    c.bench_function("&str.pad 50000 la", |b| {
+        b.iter(|| black_box("東風 ぬが ㅀㆈ".pad(width, Alignment::Left, 'µ')))
+    });
+    c.bench_function("format! 50000 la", |b| {
+        b.iter(|| black_box(format!("{:<width$}", "東風 ぬが ㅀㆈ", width = width)))
     });
 }
 
@@ -62,5 +73,6 @@ criterion_group!(
     str_pad_100_left,
     str_pad_1000_left,
     str_pad_10000_left,
-    str_pad_100000_left,
+    str_pad_25000_left,
+    str_pad_50000_left,
 );
